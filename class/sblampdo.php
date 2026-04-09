f029b32317c12a3cccf9ffa9b4c51583860664ef
lipa4r <mateusz@lipinski.wroclaw.pl>
2026-04-09 10:50:29 +0200

    Fix PDOException message building in prepareExecute

--- class/sblampdo.php
+++ class/sblampdo.php
@@ -11,8 +11,8 @@ abstract class SblamPDO extends PDO
     function prepareExecute($q,array $data)
     {
         $statement = $this->prepare($q);
-		if (!$statement) throw new PDOException(implode(',',$this->errorInfo().'// '.$q));
-		if (!$statement->execute($data)) throw new PDOException(implode(',',$statement->errorInfo().'// '.$q));
+		if (!$statement) throw new PDOException(implode(',', $this->errorInfo()).'// '.$q);
+		if (!$statement->execute($data)) throw new PDOException(implode(',', $statement->errorInfo()).'// '.$q);
 		return $statement;
     }
 
